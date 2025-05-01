f39176b03b0eac14022f7f0f8fe41d5d21ab6333
syuh23 <seonghee5070@gmail.com>
2025-05-01 17:04:29 +0900

    redis connection test 2

--- src/main/java/com/example/weup/service/RedisService.java
+++ src/main/java/com/example/weup/service/RedisService.java
@@ -1,7 +1,5 @@
 package com.example.weup.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
