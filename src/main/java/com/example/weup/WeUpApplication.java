4a74eb3f2c1dfb1629c598116cc8eae7541c45d2
syuh23 <seonghee5070@gmail.com>
2025-05-27 13:39:28 +0900

    enable spring scheduling, edit get chat messages, add logging messages

--- src/main/java/com/example/weup/WeUpApplication.java
+++ src/main/java/com/example/weup/WeUpApplication.java
@@ -2,8 +2,10 @@ package com.example.weup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WeUpApplication {
 
     public static void main(String[] args) {
