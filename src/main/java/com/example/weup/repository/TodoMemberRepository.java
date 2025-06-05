bc8495024df901a73d24161fdb7bbe9fc061a73e
badul13 <144805932+badul13@users.noreply.github.com>
2025-06-05 14:48:20 +0900

    modify todo edit logic (#38)

--- src/main/java/com/example/weup/repository/TodoMemberRepository.java
+++ src/main/java/com/example/weup/repository/TodoMemberRepository.java
@@ -3,8 +3,13 @@ package com.example.weup.repository;
 import com.example.weup.entity.TodoMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoMemberRepository extends JpaRepository<TodoMember, Long> {
     void deleteByTodo_TodoId(Long todoId);
 
     void deleteAllByTodo_TodoId(Long todoId);
+
+    List<TodoMember> findAllByTodo_TodoId(Long todoId);
+
 }
