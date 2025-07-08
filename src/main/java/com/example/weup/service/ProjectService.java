a1579664de8d56711ee0e5c32a98aa6dea837162
syuh23 <seonghee5070@gmail.com>
2025-07-08 09:43:23 +0900

    delete unused import class

--- src/main/java/com/example/weup/service/ProjectService.java
+++ src/main/java/com/example/weup/service/ProjectService.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
