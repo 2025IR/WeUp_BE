91bf44dbdb80f7d0a9c58494796aabf3e55840f3
syuh23 <seonghee5070@gmail.com>
2025-07-16 13:39:38 +0900

    Edit WebHook Logic

--- src/main/java/com/example/weup/controller/LiveKitController.java
+++ src/main/java/com/example/weup/controller/LiveKitController.java
@@ -5,7 +5,6 @@ import com.example.weup.dto.response.DataResponseDTO;
 import com.example.weup.service.LiveKitService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +23,7 @@ public class LiveKitController {
 
         log.info("요청자 : {}, enter meeting room -> start", userId);
         String liveKitToken = liveKitService.generateLiveKitToken(projectId, userId);
-        liveKitService.enterRoom(projectId, userId);
+        //liveKitService.enterRoom(projectId, userId);
 
         log.info("요청자 : {}, enter meeting room -> success", userId);
         return ResponseEntity.ok(DataResponseDTO.of(liveKitToken, "LiveKit Token 발급이 완료되었습니다."));
@@ -53,9 +52,11 @@ public class LiveKitController {
     @PostMapping("/webhook")
     public ResponseEntity<DataResponseDTO<String>> handleWebhook(@LoginUser Long userId, @RequestBody Map<String, Object> payload) {
 
+        log.info("요청자 : {}, webhook handler -> start", userId);
         String event = (String) payload.get("event");
         liveKitService.handleWebhook(payload, event);
 
+        log.info("요청자 : {}, webhook handler -> success", userId);
         return ResponseEntity.ok(DataResponseDTO.of("WebHook Event 성공"));
     }
 
