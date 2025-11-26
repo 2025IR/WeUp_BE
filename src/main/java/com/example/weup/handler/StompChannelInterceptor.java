c99ac935b07f850897c1574795ded0ccd5894be2
syuh23 <seonghee5070@gmail.com>
2025-11-26 13:04:36 +0900

    delete unused import class

--- src/main/java/com/example/weup/handler/StompChannelInterceptor.java
+++ src/main/java/com/example/weup/handler/StompChannelInterceptor.java
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Component;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
